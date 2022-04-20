b665c28fdcd11a28f370d84bf066b344ada12995
IsabelleH02 <isa.haikel@gmail.com>
2022-04-20 20:56:33 -0300

    correções

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ mydb = myclient["ProjP1"]
 mycol = mydb["customers"]
 
 
+
 while True:
     opcao = input("Opção - 1-Inserir manualmente / 2-Ler arquivo CSV e inserir no bd/ "
                   "3-Ler Dados e criar arquivo *.txt / 4-Sair: ")
@@ -12,12 +13,10 @@ while True:
         telefone = input("Digite o telefone do novo contato: ")
 
         mydict = {"nome": nome, "telefone": telefone}
-
         x = mycol.insert_one(mydict)
         break
 
     if(opcao =="2"):
-
         arquivo = open('D:\\python-SI\\dados.csv', 'r')
         lines = arquivo.readlines()
         for l in lines:
@@ -26,37 +25,25 @@ while True:
             print("Nome: " + coluna[1])
             print("Telefone: " + coluna[2])
             print("*******************************")
-
-        codigo = coluna[0]
-        nome = coluna[1]
-        telefone = coluna[2]
-
-        mydict = {"nome": nome, "codigo": codigo, "telefone": telefone}
-
-        x = mycol.insert_one(mydict)
-
-        print(x.inserted_id)
+            codigo = coluna[0]
+            nome = coluna[1]
+            telefone = coluna[2]
+            mydict = {"nome": nome, "codigo": codigo, "telefone": telefone}
+            x = mycol.insert_one(mydict)
+            print(x.inserted_id)
         break
-
-    if(opcao =="3"):
-
+    if(opcao == "3"):
+        arq = open('D:/python-SI/agenda.txt', 'w')
+        texto = []
         lst = mycol.find({},{"_id": 0})
         for x in lst:
             print(x)
-
-        arq = open('C:/tmp/agenda.txt', 'w')
-        texto = []
-        texto.append('Agenda\n')
-        texto.append('--------------- \n')
-        texto.append(x)
-        arq.writelines(texto)
+            texto.append('Agenda\n')
+            texto.append('--------------- \n')
+            texto.append(x)
+        arq.writelines(str(texto))
         arq.close()
-
-        arq = open('C:/tmp/agenda.txt', 'r')
-        texto = arq.read()
-        print(texto)
         break
-
     if (opcao =="4"):
-
+        print("Sair")
         break
